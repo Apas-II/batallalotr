69e0fb5073387aa91d770a9cb622a6eb5d1b5eee
Juan Pablo Rayeb <juampi.rayebm@gmail.com>
2022-03-29 18:21:59 -0300

    Version 1

---
+++ batallalotr.js
@@ -0,0 +1,158 @@
+function updateModifiersfuer ()
+{
+let valorfuer = document.getElementById("valorfuer").value
+
+ if (valorfuer == 102)
+document.getElementById("normalfuer").value = 35;
+else if (valorfuer == 101)
+document.getElementById("normalfuer").value = 30;
+else if (valorfuer == 100)
+document.getElementById("normalfuer").value = 25;
+else if (valorfuer <=99 && valorfuer >=90)
+document.getElementById("normalfuer").value = 20;
+else if (valorfuer <=89 && valorfuer>= 80)
+document.getElementById("normalfuer").value = 15;
+else if (valorfuer <=79 && valorfuer>= 70)
+document.getElementById("normalfuer").value = 10;
+else if (valorfuer <=69 && valorfuer>= 50)
+document.getElementById("normalfuer").value = 5;
+else if (valorfuer <=49 && valorfuer >= 1)
+document.getElementById("normalfuer").value = 0
+else if (valorfuer == 6)
+document.getElementById("normalfuer").value = 0
+else 
+document.getElementById("normalfuer").value = null
+}
+function updateModifiersagi ()
+{
+let valoragi = document.getElementById("valoragi").value
+
+ if (valoragi == 102)
+document.getElementById("normalagi").value = 35;
+else if (valoragi == 101)
+document.getElementById("normalagi").value = 30;
+else if (valoragi == 100)
+document.getElementById("normalagi").value = 25;
+else if (valoragi <=99 && valoragi >= 90)
+document.getElementById("normalagi").value = 20;
+else if (valoragi <="89" && valoragi >= 80)
+document.getElementById("normalagi").value = 15;
+else if (valoragi <=79 && valoragi >= 70)
+document.getElementById("normalagi").value = 10;
+else if (valoragi <=69 && valoragi >= 50)
+document.getElementById("normalagi").value = 5;
+else if (valoragi <=49 && valoragi >= 1)
+document.getElementById("normalagi").value = 0
+else if (valoragi == 6)
+document.getElementById("normalagi").value = 0
+else
+document.getElementById("normalagi").value = null
+}
+function updateModifiersconst()
+{
+  let valorconst = document.getElementById("valorconst").value
+
+  if (valorconst == 102)
+ document.getElementById("normalconst").value = 35;
+ else if (valorconst == 101)
+ document.getElementById("normalconst").value = 30;
+ else if (valorconst == 100)
+ document.getElementById("normalconst").value = 25;
+ else if (valorconst <=99 && valorconst >= 90)
+ document.getElementById("normalconst").value = 20;
+ else if (valorconst <="89" && valorconst >= 80)
+ document.getElementById("normalconst").value = 15;
+ else if (valorconst <=79 && valorconst >= 70)
+ document.getElementById("normalconst").value = 10;
+ else if (valorconst <=69 && valorconst >= 50)
+ document.getElementById("normalconst").value = 5;
+ else if (valorconst <=49 && valorconst >= 1)
+ document.getElementById("normalconst").value = 0
+ else if (valorconst == 6)
+ document.getElementById("normalconst").value = 0
+ else
+ document.getElementById("normalconst").value = null
+}
+function updateModifiersint()
+{
+  let valorint = document.getElementById("valorint").value
+  if (valorint == 102)
+  document.getElementById("normalint").value = 35;
+  else if (valorint == 101)
+  document.getElementById("normalint").value = 30;
+  else if (valorint == 100)
+  document.getElementById("normalint").value = 25;
+  else if (valorint <=99 && valorint >= 90)
+  document.getElementById("normalint").value = 20;
+  else if (valorint <="89" && valorint >= 80)
+  document.getElementById("normalint").value = 15;
+  else if (valorint <=79 && valorint >= 70)
+  document.getElementById("normalint").value = 10;
+  else if (valorint <=69 && valorint >= 50)
+  document.getElementById("normalint").value = 5;
+  else if (valorint <=49 && valorint >= 1)
+  document.getElementById("normalint").value = 0
+  else if (valorint == 6)
+  document.getElementById("normalint").value = 0
+  else
+  document.getElementById("normalint").value = null 
+}
+function updateModifiersintu()
+{
+  let valorintu = document.getElementById("valorintu").value
+  if (valorintu == 102)
+  document.getElementById("normalintu").value = 35;
+  else if (valorintu == 101)
+  document.getElementById("normalintu").value = 30;
+  else if (valorintu == 100)
+  document.getElementById("normalintu").value = 25;
+  else if (valorintu <=99 && valorintu >= 90)
+  document.getElementById("normalintu").value = 20;
+  else if (valorintu <="89" && valorintu >= 80)
+  document.getElementById("normalintu").value = 15;
+  else if (valorintu <=79 && valorintu >= 70)
+  document.getElementById("normalintu").value = 10;
+  else if (valorintu <=69 && valorintu >= 50)
+  document.getElementById("normalintu").value = 5;
+  else if (valorintu <=49 && valorintu >= 1)
+  document.getElementById("normalintu").value = 0
+  else if (valorintu == 6)
+  document.getElementById("normalintu").value = 0
+  else
+  document.getElementById("normalintu").value = null 
+}
+function updateModifierspres()
+{
+  let valorpres = document.getElementById("valorpres").value
+  if (valorpres == 102)
+  document.getElementById("normalpres").value = 35;
+  else if (valorpres == 101)
+  document.getElementById("normalpres").value = 30;
+  else if (valorpres == 100)
+  document.getElementById("normalpres").value = 25;
+  else if (valorpres <=99 && valorpres >= 90)
+  document.getElementById("normalpres").value = 20;
+  else if (valorpres <="89" && valorpres >= 80)
+  document.getElementById("normalpres").value = 15;
+  else if (valorpres <=79 && valorpres >= 70)
+  document.getElementById("normalpres").value = 10;
+  else if (valorpres <=69 && valorpres >= 50)
+  document.getElementById("normalpres").value = 5;
+  else if (valorpres <=49 && valorpres >= 1)
+  document.getElementById("normalpres").value = 0
+  else if (valorpres == 6)
+  document.getElementById("normalpres").value = 0
+  else
+  document.getElementById("normalpres").value = null 
+}
+function updateModifiersapa()
+{
+  let apariencia = document.getElementById("valorapa").value
+   document.getElementById("normalapa").value  = apariencia
+}
+
+
+totalfuer = document.getElementById("totalfuer").value
+totalfuer = valorfuer.value
+
+
---
+++ ejemplos/ejemplos.js
@@ -0,0 +1,9 @@
+var todos = ['Gandalf', 'Legolas', 'Peron']
+
+todos.forEach(todos => { 
+  var div = document.createElement('Div');
+  div.innerText = todos;
+  document.body.appendChild(div);
+
+})
+// con esto listo todos los pjs 
